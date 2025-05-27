99bce5517b60291da0091755bf9205881477e6aa
josdem <joseluis.delacruz@gmail.com>
2025-05-27 13:32:09 -0600

    Setting version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
