0dc3be76cdb5a79b19664204afa621b80300b966
josdem <joseluis.delacruz@gmail.com>
2025-06-15 16:00:19 -0600

    #2 Moving fab to the second fragment

--- app/src/main/java/com/josdem/vetlog/FirstFragment.kt
+++ app/src/main/java/com/josdem/vetlog/FirstFragment.kt
@@ -24,7 +24,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
 import com.josdem.vetlog.databinding.FragmentFirstBinding
 import com.josdem.vetlog.service.RetrofitHelper
 import com.josdem.vetlog.service.VetlogService
@@ -46,15 +45,6 @@ class FirstFragment : Fragment() {
     ): View {
         vetlogService = RetrofitHelper.getInstance().create(VetlogService::class.java)
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar
-                .make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab)
-                .show()
-        }
-
         return binding.root
     }
 
