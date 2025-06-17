af900b579190eb2b0e6fd62be11e68478f91b7c6
josdem <joseluis.delacruz@gmail.com>
2025-06-17 05:04:42 -0600

    #1 Sending pulling up message for each pet in the list

--- app/src/main/java/com/josdem/vetlog/helper/DialogHelper.kt
+++ app/src/main/java/com/josdem/vetlog/helper/DialogHelper.kt
@@ -1,19 +1,47 @@
+/*
+  Copyright 2025 Jose Morales contact@josdem.io
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*/
+
 package com.josdem.vetlog.helper
 
 import android.app.AlertDialog
 import android.content.Context
+import android.util.Log
+import com.josdem.vetlog.service.RetrofitHelper
+import com.josdem.vetlog.service.VetlogService
 import com.josdem.vetlog.state.ApplicationState
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class DialogHelper(
     private val context: Context,
 ) {
     private val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+    private var vetlogService = RetrofitHelper.getInstance().create(VetlogService::class.java)
 
     init {
         builder
             .setTitle("Send pulling up message")
             .setPositiveButton("Send") { dialog, which ->
-                // Do something.
+                MainScope().launch {
+                    ApplicationState.getValue("petIds")?.forEach {
+                        val result = vetlogService.pullingUp(it)
+                        Log.d("response: ", result.body().toString())
+                    }
+                }
             }.setNegativeButton("Close") { dialog, which ->
                 // Do something else.
             }.setMultiChoiceItems(
