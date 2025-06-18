54cb5ee47bc4405453362ae0cd677f6f6c23ed04
josdem <joseluis.delacruz@gmail.com>
2025-06-18 10:48:13 -0600

    #8 Updating version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.josdem.vetlog"
         minSdk = 23
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
