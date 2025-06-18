eeef319cf66374438a96783dfb4a9fcc2d543c2d
josdem <joseluis.delacruz@gmail.com>
2025-06-17 18:56:32 -0600

    #5 Completing main navigation test

--- app/src/androidTest/java/com/josdem/vetlog/MainNavigationTest.kt
+++ app/src/androidTest/java/com/josdem/vetlog/MainNavigationTest.kt
@@ -18,7 +18,10 @@
 package com.josdem.vetlog
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -35,5 +38,9 @@ class MainNavigationTest {
     @Test
     fun mainNavigationTest() {
         onView(withId(R.id.petIds)).perform(typeText("338"))
+        onView(withId(R.id.send)).perform(click())
+
+        val secondFragment = onView(withId(R.id.SecondFragment))
+        secondFragment.check(matches(isDisplayed()))
     }
 }
