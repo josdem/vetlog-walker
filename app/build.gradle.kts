6eab724161f17c2a128421b7e53f1defde7607bf
josdem <joseluis.delacruz@gmail.com>
2025-06-18 15:22:49 -0600

    #3 Updating spotless to the latest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.diffplug.spotless") version "7.0.3"
+    id("com.diffplug.spotless") version "7.0.4"
 }
 
 android {
