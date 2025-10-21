95c020828bffdcf4519790fdc3d9d2bdfc8d40f0
josdem <joseluis.delacruz@gmail.com>
2025-10-21 12:25:34 -0600

    #23 Updating project version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 10
-        versionName = "1.1.3"
+        versionName = "1.1.4"
         buildConfigField("String", "TOKEN",  project.findProperty("token") as String)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
